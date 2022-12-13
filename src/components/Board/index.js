c2271751f3fc5bf0ad91632cc16308e17a592ce0
mmoscosa <mmoscosa@gmail.com>
2022-12-13 09:02:04 -0600

    adding win95 css

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,127 +1,266 @@
-import React, { useEffect, useState } from 'react'
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyles } from './BoardElements'
-import { Button, Window, WindowContent, WindowHeader } from 'react95'
+import React from 'react'
+import PropTypes from 'prop-types'
+import Cell from '../Cell'
 
-// original Windows95 font
-import original from 'react95/dist/themes/original'
+import './BoardElements.css'
 
-// Create an empty 9 by 9 matrix
-const defaultMatrix = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
+class Board extends React.Component {
+  state = this.getInitialState()
 
-const Board = () => {
-  const [matrix, setMatrix] = useState(defaultMatrix)
-  const [gameStarted, setGameStarted] = useState(false)
-  const blankState = 0
-  const revealMines = (clickedRow, clickedCol) => {
-    const m = JSON.parse(JSON.stringify(matrix))
-    // if clicked box is empty
-    if (m[clickedRow][clickedCol] === blankState) {
-      // find the neighbouring cells containing
-      const neighbours = getNeighbours(clickedRow, clickedCol)
-      m[clickedRow][clickedCol] = '0'
-      setMatrix(m)
-      neighbours.forEach((element) => {
-        revealMines(element[0], element[1])
-        setMatrix(m)
-      })
+  getInitialState() {
+    const initialState = {
+      grid: this.createNewBoard(),
+      minesCount: this.props.mines,
+      gameStatus: this.props.gameStatus,
+      revealedCells: 0,
     }
+    return initialState
   }
 
-  const getNeighbours = (y, x) => {
-    const neighbours = []
-    const currentRow = matrix[y]
-    const prevRow = matrix[y - 1]
-    const nextRow = matrix[y + 1]
+  // Board utilities
+  createNewBoard(click = null) {
+    const grid = []
+    const rows = this.props.width
+    const columns = this.props.height
+    const minesCount = this.props.mines
+    const minesArray = this.getRandomMines(minesCount, columns, rows, click)
 
-    if (currentRow[x - 1]) neighbours.push([y, x - 1])
-    if (currentRow[x + 1]) neighbours.push([y, x + 1])
+    for (let i = 0; i < columns; ++i) {
+      grid.push([])
+      for (let j = 0; j < rows; ++j) {
+        const gridCell = new GridCell(i, j, minesArray.includes(i * rows + j))
+        this.addGridCell(grid, gridCell)
+      }
+    }
+
+    return grid
+  }
+
+  getRandomMines(amount, columns, rows, starter = null) {
+    const minesArray = []
+    const limit = columns * rows
+    const minesPool = [...Array(limit).keys()]
+
+    if (starter > 0 && starter < limit) {
+      minesPool.splice(starter, 1)
+    }
+
+    for (let i = 0; i < amount; ++i) {
+      const n = Math.floor(Math.random() * minesPool.length)
+      minesArray.push(...minesPool.splice(n, 1))
+    }
+
+    return minesArray
+  }
+
+  addGridCell(grid, gridCell) {
+    const y = grid.length - 1
+    const x = grid[y].length
+    const lastGridCell = gridCell
+    const neighbours = this.getNeighbours(grid, y, x)
+
+    for (let neighbourGridCell of neighbours) {
+      if (lastGridCell.isMine) {
+        neighbourGridCell.n += 1
+      } else if (neighbourGridCell.isMine) {
+        lastGridCell.n += 1
+      }
+    }
+
+    grid[y].push(gridCell)
+  }
+
+  revealBoard() {
+    const grid = this.state.grid
+
+    for (const row of grid) {
+      for (const gridCell of row) {
+        gridCell.isRevealed = true
+      }
+    }
+
+    this.setState({})
+  }
+
+  restartBoard() {
+    this.setState(this.getInitialState())
+  }
+
+  /* Helpers */
+  getNeighbours(grid, y, x) {
+    const neighbours = []
+    const currentRow = grid[y]
+    const prevRow = grid[y - 1]
+    const nextRow = grid[y + 1]
+
+    if (currentRow[x - 1]) neighbours.push(currentRow[x - 1])
+    if (currentRow[x + 1]) neighbours.push(currentRow[x + 1])
     if (prevRow) {
-      if (prevRow[x - 1]) neighbours.push([y - 1, x - 1])
-      if (prevRow[x]) neighbours.push([y - 1, x])
-      if (prevRow[x + 1]) neighbours.push([y - 1, x + 1])
+      if (prevRow[x - 1]) neighbours.push(prevRow[x - 1])
+      if (prevRow[x]) neighbours.push(prevRow[x])
+      if (prevRow[x + 1]) neighbours.push(prevRow[x + 1])
     }
     if (nextRow) {
-      if (nextRow[x - 1]) neighbours.push([y + 1, x - 1])
-      if (nextRow[x]) neighbours.push([y + 1, x])
-      if (nextRow[x + 1]) neighbours.push([y + 1, x + 1])
+      if (nextRow[x - 1]) neighbours.push(nextRow[x - 1])
+      if (nextRow[x]) neighbours.push(nextRow[x])
+      if (nextRow[x + 1]) neighbours.push(nextRow[x + 1])
     }
 
     return neighbours
   }
 
-  const putMines = (clickedRow, clickedCol, m) => {
-    let minesPlaced = 0
-    while (minesPlaced < 10) {
-      // Generate random coordinates for the mine
-      const row = Math.floor(Math.random() * 9)
-      const col = Math.floor(Math.random() * 9)
-      // avoid  mine in first clicked box
-      if (clickedRow === row && clickedCol === col) {
+  revealEmptyNeigbhours(grid, y, x) {
+    const neighbours = [...this.getNeighbours(grid, y, x)]
+    grid[y][x].isFlagged = false
+    grid[y][x].isRevealed = true
+
+    while (neighbours.length) {
+      const neighbourGridCell = neighbours.shift()
+
+      if (neighbourGridCell.isRevealed) {
         continue
       }
-      // Check if the cell at the given coordinates is empty
-      if (m[row][col] === blankState) {
-        // Place the mine in the cell
-        m[row][col] = '💣'
-        minesPlaced++
+      if (neighbourGridCell.isEmpty) {
+        neighbours.push(
+          ...this.getNeighbours(grid, neighbourGridCell.y, neighbourGridCell.x)
+        )
       }
+
+      neighbourGridCell.isFlagegd = false
+      neighbourGridCell.isRevealed = true
     }
-    return m
   }
 
-  const checkMine = (clickedRow, clickedCol) => {
-    if (!gameStarted) {
-      setGameStarted(true)
-      const mines = putMines(clickedRow, clickedCol, matrix)
-      setMatrix(mines)
+  checkVictory() {
+    const { height, width, mines } = this.props
+    const revealed = this.getRevealed()
+
+    if (revealed >= height * width - mines) {
+      this.killBoard('win')
     }
-    revealMines(clickedRow, clickedCol)
   }
 
-  useEffect(() => {}, [matrix])
+  getRevealed = () => {
+    return this.state.grid
+      .reduce((r, v) => {
+        r.push(...v)
+        return r
+      }, [])
+      .map((v) => v.isRevealed)
+      .filter((v) => !!v).length
+  }
 
-  return (
-    <>
-      <GlobalStyles />
-      <ThemeProvider theme={original}>
-        <Window className="window">
-          <WindowHeader className="window-title">
-            <span>Sunroom Minesweeper</span>
-          </WindowHeader>
-          <WindowContent>
-            {matrix.map((rows, rindex) => {
-              return (
-                <div key={rindex} style={{ display: 'block' }}>
-                  {rows.map((cols, cindex) => {
-                    return (
-                      <Button
-                        active={cols === '0'}
-                        key={`${rindex},${cindex}`}
-                        onClick={() => {
-                          checkMine(rindex, cindex)
-                        }}
-                      />
-                    )
-                  })}
-                </div>
-              )
-            })}
-          </WindowContent>
-        </Window>
-      </ThemeProvider>
-    </>
-  )
+  killBoard(type) {
+    const message = type === 'lost' ? 'You lost.' : 'You won.'
+
+    this.setState({ gameStatus: message }, () => {
+      this.revealBoard()
+    })
+  }
+
+  // Cell click handlers
+  handleLeftClick(y, x) {
+    const grid = this.state.grid
+    const gridCell = grid[y][x]
+
+    gridCell.isClicked = true
+
+    // Might want to add an "isUnknown" state later
+    if (gridCell.isRevealed || gridCell.isFlagged) {
+      return false
+    }
+
+    // End game if mine
+    if (gridCell.isMine) {
+      this.killBoard('lost')
+      return false
+    }
+
+    if (gridCell.isEmpty) {
+      this.revealEmptyNeigbhours(grid, y, x)
+    }
+
+    gridCell.isFlagged = false
+    gridCell.isRevealed = true
+
+    this.setState({}, () => {
+      this.checkVictory()
+    })
+  }
+
+  // Cell right-click handler
+  handleRightClick(e, y, x) {
+    e.preventDefault()
+    const grid = this.state.grid
+    let minesLeft = this.state.minesCount
+
+    // Check if already revealed
+    if (grid[y][x].isRevealed) return false
+
+    if (grid[y][x].isFlagged) {
+      grid[y][x].isFlagged = false
+      minesLeft++
+    } else {
+      grid[y][x].isFlagged = true
+      minesLeft--
+    }
+
+    this.setState({
+      minesCount: minesLeft,
+    })
+  }
+
+  // Rendering functions
+  renderBoard() {
+    const grid = this.state.grid
+
+    return grid.map((row) => {
+      const rowCells = row.map((gridCell) => (
+        <Cell
+          key={gridCell.y * row.length + gridCell.x}
+          onClick={() => this.handleLeftClick(gridCell.y, gridCell.x)}
+          cMenu={(e) => this.handleRightClick(e, gridCell.y, gridCell.x)}
+          value={gridCell}
+        />
+      ))
+
+      return <div className="row">{rowCells}</div>
+    })
+  }
+
+  render() {
+    return (
+      <div className="board">
+        <div className="mines-count">
+          <span>Mines: {this.state.minesCount}</span>
+        </div>
+        <div className="grid">{this.renderBoard()}</div>
+      </div>
+    )
+  }
+}
+
+class GridCell {
+  constructor(y, x, isMine) {
+    this.x = x
+    this.y = y
+    this.n = 0
+    this.isMine = isMine
+    this.isRevealed = false
+    this.isFlagged = false
+    this.isUnknown = false
+    this.isClicked = false
+  }
+  get isEmpty() {
+    return this.n === 0 && !this.isMine
+  }
+}
+
+// Type checking With PropTypes
+Board.propTypes = {
+  height: PropTypes.number,
+  width: PropTypes.number,
+  mines: PropTypes.number,
 }
 
 export default Board
---
+++ src/components/Minesweeper/index.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import Board from '../Board'
+
+import './MinesweeperElements.css'
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.boardElement = React.createRef()
+
+    this.state = {
+      height: 9,
+      width: 9,
+      mines: 10,
+      gameStatus: 0,
+    }
+  }
+
+  handleChange = (prop, value) => {
+    this.setState({ [prop]: value })
+  }
+
+  // From https://dash.harvard.edu/bitstream/handle/1/14398552/BECERRA-SENIORTHESIS-2015.pdf article we set the levels and complexity
+  handleChangeHeight = (event) => {
+    const val = clamp(event.target.value, 5, 16)
+    this.handleChange('height', val)
+  }
+
+  handleChangeWidth = (event) => {
+    const val = clamp(event.target.value, 5, 30)
+    this.handleChange('width', val)
+  }
+
+  handleChangeMines = (event) => {
+    // Hardest game may have 99 mines, but otherwise customizable to anything between a math.floor from height and idth divided by 3
+    let cap = Math.floor((this.state.height * this.state.width) / 3)
+    cap = cap < 99 ? cap : clamp(event.target.value, 5, 99)
+    const val = clamp(event.target.value, 1, cap)
+    this.handleChange('mines', val)
+  }
+
+  restartGame = () => {
+    this.boardElement.current.restartBoard()
+  }
+
+  render() {
+    const { height, width, mines, gameStatus } = this.state
+    return (
+      <div className="game">
+        <Board
+          ref={this.boardElement}
+          height={height}
+          width={width}
+          mines={mines}
+          gameStatus={gameStatus}
+        />
+        <div className="control-buttons">
+          <button
+            onClick={this.restartGame}
+            class="btn btn-lg btn-block sunroom-btn"
+          >
+            Restart
+          </button>
+
+          <form>
+            <label>Height</label>
+            <input
+              type="number"
+              value={this.state.height}
+              onChange={this.handleChangeHeight}
+            />
+            <label>Width</label>
+            <input
+              type="number"
+              value={this.state.width}
+              onChange={this.handleChangeWidth}
+            />
+            <label>Mines</label>
+            <input
+              type="number"
+              value={this.state.mines}
+              onChange={this.handleChangeMines}
+            />
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+function clamp(n, min, max) {
+  return Math.max(min, Math.min(n, max))
+}
+
+export default Game
